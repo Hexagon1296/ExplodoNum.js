9c46b7f758f39964c5fcbb7ab29407e096e14647
30IGithub <54778430+Hexagon1296@users.noreply.github.com>
2024-01-16 19:44:11 -0500

    Create OmegaExpantaNum.js

---
+++ OmegaExpantaNum.js
@@ -0,0 +1,16 @@
+;(function(globalScobe){
+  // Editable defaults //
+  var OmegaExpantaNum = {
+    maxOps: 1000, // Maximum number of operations, lower = better but less accurate, hih
+    serializeMode: 0
+  //debug: 0
+  },
+    external = true,
+    omegaExpantaError = "[OENError]",
+    invalidArgument = `${omegaExpantaError} Invalid argument:`,
+    MAX_SAFE_INTEGER = 9007199254740991,
+    MAX_E = Math.log10(MAX_SAFE_INTEGER),
+    P = {},
+    Q = {},
+    R = {};
+})(this)
